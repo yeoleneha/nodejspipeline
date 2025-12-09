65d6da3ab21a39f544996f5b8c30eb5ba4b06fd1
neha <nehayeole213@gmail.com>
2025-12-09 11:13:57 +0000

    cm4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express')i;
+                var express = require('express')i;
                  
                 var app = express();//Respond with "hello world" for requests that hit our root "/"
                 app.get('/', function (req, res) {
-                 res.send('welcome to Jenkins server! ');
+                 res.send('welcome too Jenkin server! ');
                 });//listen to port 3000 by default
                 app.listen(process.env.PORT || 3000);
                  
